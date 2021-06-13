2f3e305b323e0063b51d7c93126e15e7a96412a6
Richard Lewis <richard@rjlewis.me.uk>
2021-06-13 09:32:44 +0100

    Initialise SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+
+    let window = video.window("Flappy Dragon", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.clear();
+    canvas.present();
 }
