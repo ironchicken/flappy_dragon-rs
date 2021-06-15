40f6b712a888e2d00b6c871ac8d3c30fc9ef2609
Richard Lewis <richard@rjlewis.me.uk>
2021-06-15 22:52:14 +0100

    Add game loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 extern crate sdl2;
 
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+const FPS: u32 = 60;
+const FRAME_DELAY: u32 = 1000 / FPS;
+
+fn game_loop(mut timer: sdl2::TimerSubsystem, canvas: &Canvas<Window>) {
+    loop {
+	let frame_start = timer.ticks();
+
+	let frame_time = timer.ticks() - frame_start;
+	if frame_time < FRAME_DELAY {
+	    timer.delay(FRAME_DELAY - frame_time);
+	}
+    }
+}
+
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
+    let timer = sdl_context.timer().unwrap();
 
-    let window = video.window("Flappy Dragon", 800, 600)
+    let window = video.window("Flappy Dragon", WIDTH, HEIGHT)
         .position_centered()
         .build()
         .unwrap();
@@ -13,4 +33,6 @@ fn main() {
 
     canvas.clear();
     canvas.present();
+
+    game_loop(timer, &canvas);
 }
