96e00ff6915cc5ccbde1e3e699f8a0fc9812118d
Richard Lewis <richard@rjlewis.me.uk>
2021-06-17 12:59:46 +0100

    Draw player

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 
@@ -18,8 +20,26 @@ enum GameState {
 }
 
 struct Player {
-    x: u32,
-    y: u32,
+    x: i32,
+    y: i32,
+}
+
+const PLAYER_WIDTH: u32 = 32;
+const PLAYER_HEIGHT: u32 = 32;
+const PLAYER_COLOR: Color = Color::RGB(255, 0, 0);
+
+trait Sprite {
+    fn draw(&self, canvas: &mut Canvas<Window>);
+}
+
+impl Sprite for Player {
+    fn draw(&self, canvas: &mut Canvas<Window>) {
+	let r = sdl2::rect::Rect::new(self.x + (PLAYER_WIDTH as i32 / 2),
+				      self.y - (PLAYER_HEIGHT as i32 / 2),
+				      PLAYER_WIDTH, PLAYER_HEIGHT);
+	canvas.set_draw_color(PLAYER_COLOR);
+	canvas.fill_rect(r).unwrap();
+    }
 }
 
 fn update(game_state: &mut GameState, player: &mut Player) {
@@ -27,6 +47,10 @@ fn update(game_state: &mut GameState, player: &mut Player) {
 
 fn render(canvas: &mut Canvas<Window>, game_state: &mut GameState, player: &mut Player) {
     canvas.clear();
+
+    player.draw(canvas);
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.present();
 }
 
@@ -86,7 +110,7 @@ fn main() {
     let mut events = sdl_context.event_pump().unwrap();
 
     let mut game_state = GameState::Menu;
-    let mut player = Player { x: 0, y: WIDTH / 2 };
+    let mut player = Player { x: 0, y: HEIGHT as i32 / 2 };
 
     game_loop(&mut game_state, &mut player, &mut canvas, &mut timer, &mut events);
 }
